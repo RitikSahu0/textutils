7415635d8c5084979cedc5daf30416491271e3fc
Ritik Sahu <Ritiksahu00980@gmail.com>
2023-04-21 23:15:30 +0530

    propsSolution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Invitation } from './invitation/invitation';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      < Invitation party ="Birthday party Invitation"
+      email = "jaGdish@gmail.com"
+  hey = "jaGdish" 
+  birthday = "birthday party"
+  names ="Ritu , Saurabh , Kartik"
+  address = "Green field Avenue"
+  from = "JaGdish"
+      />
+
+
+
+
+    
+  
+       
     </div>
   );
 }
